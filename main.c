f1c5f77d23c7c715b79f5d3678515d885e88dbfb
Iñaki Mir <imirbelloso@gmail.com>
2025-09-24 13:58:49 +0200

    Exercici birthday

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
-void main(){
-    
+int main() {
+    int day, month, year;
+
+    printf("Introdueix el teu dia de naixement:\n");
+    scanf("%d",&day);
+
+    printf("Introdueix el teu mes de naixement:\n");
+    scanf("%d",&month);
+
+    printf("Introdueix el teu any de naixement (4 digits):\n");
+    scanf("%d",&year);
+
+    int result = day + month + year;
+    printf("El resultat és: %d\n",result);
 }
